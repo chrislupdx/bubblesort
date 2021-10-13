c197b749f11f6b528cb5ffe5b1319647610ad341
Christopher Lu <chlu@rita.cecs.pdx.edu>
2021-10-13 14:50:33 -0700

    off by one did

--- exitloop.py
+++ exitloop.py
@@ -1,17 +1,16 @@
 #this is an attempt to code bubble sort in python, the goal is to implement a working bubble sort
 #then check whether my designed escape works with the scope manipulation I am trying.
-import numpy as np
 
 #Standard Bubble Sort
 def bblSort(A):
     print("bb start")
-    for i in range(len(A) - 2): #-1 or -2?
-        for x in range(len(A) - 2 - i):
+    for i in range(len(A) - 1): #-1 or -2? why can't we touch the last item
+        for x in range(len(A) - 1 - i):
+            print("A[x + 1] =", A[x + 1], " A[x] =" , A[x])
             if A[x + 1] < A[x]:
                swap(A, x + 1, x)
     print("bb end")
     return A
-
 
 #Swap Function
 def swap(A, pos1, pos2):
